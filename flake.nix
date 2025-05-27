4244c4124947ed0cbdad2845714b3361f66dd6db
ElrohirGT <elrohirgt@gmail.com>
2025-05-26 18:30:47 -0600

    feat: Added Nix ambient

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "A basic multiplatform flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    # System types to support.
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    # Nixpkgs instantiated for supported system types.
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+      python = pkgs.python3.withPackages (p:
+        with p; [
+          uvicorn
+          requests
+          fastapi
+          python-dotenv
+          pymongo
+
+          streamlit
+          pandas
+        ]);
+    in {
+      default = pkgs.mkShell {
+        packages = [python];
+      };
+    });
+  };
+}
